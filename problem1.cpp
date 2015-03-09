6ff2303da6ea372b284cb7ca2ecb78a6a09d4a75
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2015-03-10 04:04:47 +0900

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,21 @@
+// g++ -std=c++11 problem1.cpp && ./a.exe 
+
+#include <iostream>
+
+int problem1(int N)
+{
+    int result = 0;
+    for (int i = 1; i < N; ++i) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            result += i;
+        }
+    }
+    return result;
+}
+
+int main(int argc, char const* argv[])
+{
+    std::cout << "result (N=10):   " << problem1(10)   << std::endl;
+    std::cout << "result (N=1000): " << problem1(1000) << std::endl;
+    return 0;
+}
