d72d6b347aa4e3a0a92b853bdcaf3dd0ce1cef79
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2015-03-13 02:52:51 +0900

    fix problem6

--- problem6.cpp
+++ problem6.cpp
@@ -5,6 +5,11 @@
 // License:  MIT License
 // Created:  2015-03-13T01:28:19
 
+// output:
+// result = 22
+// result = 350
+// result = 25164150
+
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -19,12 +24,16 @@ long sum_of_square(int n)
 
 long square_of_sum(int n)
 {
-    long tmp = static_cast<long>(n + 1) * (n/2 + n%2);
+    long tmp = static_cast<long>(n + 1 + n%2)*(n/2) + (n%2);
     return tmp*tmp;
 }
 
+inline long problem6(int n) { return square_of_sum(n) - sum_of_square(n); }
+
 int main(int argc, char** argv)
 {
-    cout << "result = " << square_of_sum(100) - sum_of_square(100) << endl;
+    cout << "result = " << problem6(3) << endl;
+    cout << "result = " << problem6(6) << endl;
+    cout << "result = " << problem6(100) << endl;
 }
 
