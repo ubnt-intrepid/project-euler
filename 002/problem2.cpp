fcbec39437c9e3a1862e552cb76202cdbaaf24cd
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2015-06-23 19:12:31 +0900

    Update problem2.cpp

--- 002/problem2.cpp
+++ 002/problem2.cpp
@@ -1,6 +1,6 @@
 // g++ -std=c++11 problem2.cpp && ./a.exe
 // output:
-// sum = 4613731
+// sum = 4613732
 
 #include <iostream>
 
@@ -8,7 +8,7 @@ int problem2(int num)
 {
     int sum = 0;
     for (int f1 = 1, f2 = 1; f2 < num; f2 += f1, f1 = f2 - f1) {
-        if (f2 % 2 != 0)
+        if (f2 % 2 == 0)
             sum += f2;
     }
     return sum;
