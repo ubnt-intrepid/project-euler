0e8ded7f1a4a547a8029b574339abb67230fc9df
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2015-06-25 21:36:52 +0900

    Update 004.py

--- 004/004.py
+++ 004/004.py
@@ -1,12 +1,11 @@
-from itertools import product
-
 def num_of_digits(val, base):
-    " 基数 base のもとでの桁数を求める "
+    " 基数 base のもとでの桁数 "
     n = 1
     while int(val / base) > 0:
         val /= base 
         n += 1
     return n
+
 
 def isPalindrome(val, base=10):
     def digit(d):
@@ -14,12 +13,13 @@ def isPalindrome(val, base=10):
         return int(val / (base**d)) % base 
 
     n = num_of_digits(val, base)
+    return all(digit(i) == digit(n - 1 - i) for i in range(int(n / 2)))
 
-    return all(digit(i) == digit(n - 1 - i) for i in range(int(n/2)))
 
-p1,p2=0,0
-for i, j in product(reversed(range(100, 1000)), repeat=2):
-    if isPalindrome(i*j) and p1*p2 < i*j:
-        p1,p2=i,j
+p1, p2 = 0, 0
+for i in range(100, 1000):
+    for j in range(i, 1000):
+        if isPalindrome(i*j) and p1 * p2 < i * j:
+            p1, p2 = i, j
 
-print(p1*p2, '=', p1, 'x', p2)
+print(p1 * p2, '=', p1, 'x', p2)
